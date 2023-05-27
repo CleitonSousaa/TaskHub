e188682032cebad07811a85961ff5c3b95d19c35
CleitonSousaa <fccleitonfs.329@gmail.com>
2023-05-27 16:58:11 -0300

    Salvar Progresso
    Estilização da pagina de projetos
    e criação da lógica para determinado evento

--- costs/src/components/pages/Projetos.js
+++ costs/src/components/pages/Projetos.js
@@ -1,9 +1,32 @@
+import {useLocation} from 'react-router-dom'
 
+import Message from "../layout/message"
+import Conteiner from "../layout/Container"
+import LinkBottun from '../layout/LinkButton'
 
+import styles from './Projetos.module.css'
 
 
 function Projetos(){
-    return <h1>projetos</h1>
+
+    const location = useLocation()
+    let message = ''
+    if(location.state){
+        message = location.state.message
+    }
+
+    return (
+        <div className={styles.project_conteiner}>
+            <div className={styles.title_conteiner}>
+               <h1>Projetos</h1> 
+               <LinkBottun to="/NovoProjeto" text="Criar Projetos"/>
+            </div>
+            {message && <Message type="success" msg={message}  /> }
+            <Conteiner customClass="start">
+                <p>Projetos...</p>
+            </Conteiner>
+        </div>
+    )
     
 }
 export default Projetos
