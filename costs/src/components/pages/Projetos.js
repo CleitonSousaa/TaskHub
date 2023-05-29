b870957f19a74d79a605a7c3304b58d39b54f108
CleitonSousaa <fccleitonfs.329@gmail.com>
2023-05-29 12:04:43 -0300

    Salvar Progresso
    Criação e estilização de componentes de exibição dos dados que vem da api

--- costs/src/components/pages/Projetos.js
+++ costs/src/components/pages/Projetos.js
@@ -1,19 +1,41 @@
 import {useLocation} from 'react-router-dom'
+import { useState, useEffect } from 'react'
 
 import Message from "../layout/message"
 import Conteiner from "../layout/Container"
 import LinkBottun from '../layout/LinkButton'
 
 import styles from './Projetos.module.css'
+import ProjetosCard from '../project/ProjetosCard'
+
 
 
 function Projetos(){
+
+    const [Projetos, setProjetos] = useState([])
+
 
     const location = useLocation()
     let message = ''
     if(location.state){
         message = location.state.message
     }
+
+
+    useEffect(() => {
+
+        fetch('http://localhost:5001/projects', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+        }).then(resp => resp.json())
+        .then(data => {
+            console.log(data)
+            setProjetos(data)
+        })
+        .catch(err => console.log(err))
+    }, [])
 
     return (
         <div className={styles.project_conteiner}>
@@ -23,7 +45,17 @@ function Projetos(){
             </div>
             {message && <Message type="success" msg={message}  /> }
             <Conteiner customClass="start">
-                <p>Projetos...</p>
+                {Projetos.length > 0 && 
+                Projetos.map((Projetos) => (
+                    <ProjetosCard   id={Projetos.id}
+                                    name={Projetos.name}
+                                    budget={Projetos.budget}
+                                    category={Projetos.category.name}
+                                    key={Projetos.id}
+
+                                     />
+                ))}
+                
             </Conteiner>
         </div>
     )
